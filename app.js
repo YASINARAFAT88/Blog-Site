8f34f03d3f8a845c5628a4cebd5683a7b5d9709d
YASINARAFAT8 <yasin80545859@gmail.com>
2022-09-16 20:08:05 +0600

    Blog Writing section Complate

--- app.js
+++ app.js
@@ -22,3 +22,55 @@ function closeFullImg() {
 }
 
 // Blog Adding Part 
+
+let submit = document.querySelector('.submit');
+let name = document.getElementById('name')
+let feedback = document.getElementById('feedback')
+let feedbackElem = document.querySelector('.feedback')
+let feedbacks = JSON.parse(localStorage.getItem('feedback'))
+if (feedback) {
+    feedbacks.forEach(element => {
+        addfeedback(element)
+    });
+}
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    addfeedback()
+});
+
+function addfeedback(obj) {
+    let nameValue = name.value;
+    let feedbackValue = feedback.value;
+    if (obj) {
+        nameValue = obj.name;
+        feedbackValue = obj.feedback
+    }
+    let card = document.createElement('div')
+
+    card.classList.add('card')
+    if (nameValue && feedbackValue) {
+        card.innerHTML = ` <h4>${nameValue}</h4>
+    <p>${feedbackValue}</p>
+    <button class="del">Delete</button>`;
+        feedbackElem.appendChild(card);
+        updatesLs()
+    };
+
+    let del = card.querySelector('.del');
+    del.addEventListener("click", () => {
+        card.remove()
+        updatesLs()
+    })
+}
+
+function updatesLs() {
+    card = document.querySelectorAll('.card')
+    let arr = [];
+    card.forEach(element => {
+        arr.push({
+            name: element.children[0].innerText,
+            feedback: element.children[1].innerText
+        })
+    });
+    localStorage.setItem("feedback", JSON.stringify(arr))
+}
